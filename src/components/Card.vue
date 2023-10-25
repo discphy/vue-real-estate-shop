8582c2137edc31b9a52b4dbf3ee99a66330bc659
discphy <dinophy@nate.com>
2023-10-26 00:14:51 +0900

    feat(add) : vue

---
+++ src/components/Card.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <img :src="room.image" alt="" class="room-img">
+    <h4 @click="$emit('openModal', room)">{{ room.title }}</h4>
+    <p>{{ room.price }}원</p>
+  </div>
+</template>
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Card',
+  props : {
+    room : Object
+  }
+}
+</script>
+
+<style>
+
+</style>
