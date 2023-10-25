6dfd0b1b78874a4abbed34ffa17eb8d18e06bd24
discphy <dinophy@nate.com>
2023-10-26 00:14:51 +0900

    feat(add) : vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,164 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <transition name="fade">
+      <Modal @closeModal="modalClose" :modalRoom="modalRoom" :modal_status="modal_status"/>
+    </transition>
+
+    <div class="menu">
+      <a v-for="menu in menus" :key="menu">{{ menu }}</a>
+    </div>
+
+    <Discount :discount="discount"/>
+
+    <button @click="priceSort">가격순 정렬</button>
+    <button @click="initSort">초기화</button>
+
+    <Card @openModal="modalOpen(room)" v-for="room in rooms" :key="room" :room="room"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import data from './assets/data.js';
+import Discount from "@/components/Discount.vue";
+import Modal from "@/components/Modal.vue";
+import Card from "@/components/Card.vue";
 
 export default {
   name: 'App',
+  data() {
+    return {
+      discount : 30,
+      showDiscount : true,
+      originRooms : [...data],
+      obj : { name : 'Kim', age : 20 },
+      modalRoom : {},
+      rooms : data,
+      modal_status : false,
+      menus : [ 'Home', 'Shop', 'About' ],
+      products : [{
+        img : './assets/room0.jpg',
+        name : '역삼동원룸',
+        price : 50,
+        count : 0
+      }, {
+        img : './assets/room1.jpg',
+        name : '천호동원룸',
+        price : 60,
+        count : 0
+      }, {
+        img : './assets/room2.jpg',
+        name : '마포구원룸',
+        price : 70,
+        count : 0
+      }]
+    }
+  },
+  methods : {
+    increase(product) {
+      product.count++;
+    },
+    modalOpen(room) {
+      this.modalRoom = room;
+      this.modal_status = true;
+    },
+    modalClose() {
+      this.modal_status = false;
+    },
+    priceSort() {
+      this.rooms.sort(function (a, b) {
+        return a.price - b.price;
+      })
+    },
+    initSort() {
+      this.rooms = [...this.originRooms];
+    }
+  },
+  mounted() {
+    setInterval(() => {
+      this.discount--;
+    }, 1000);
+  },
   components: {
-    HelloWorld
+    Discount : Discount,
+    Modal : Modal,
+    Card : Card,
   }
 }
 </script>
 
 <style>
+.fade-enter-from {
+  opacity: 0;
+}
+
+.fade-enter-active {
+  transition: all 1s;
+}
+
+.fade-enter-to {
+  opacity: 1;
+}
+
+body {
+  margin : 0
+}
+
+div {
+  box-sizing: border-box;
+}
+
+.discount {
+  background: #eee;
+  padding: 10px;
+  margin: 10px;
+  border-radius: 10px;
+}
+
+.black-bg {
+  width: 100%;
+  height: 100%;
+  background: rgba(0,0,0,0.5);
+  position: fixed;
+  padding: 20px;
+}
+
+.white-bg {
+  width: 100%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+
+.room-img {
+  width: 100%;
+  margin-top: 40px;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+
+.menu a {
+  color: white;
+  padding: 10px;
+}
+
+.start {
+  opacity: 0;
+  transition: all 1s;
+}
+
+.end {
+  opacity: 1;
 }
 </style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,48 @@
+<template>
+  <div v-if="modal_status" class="black-bg">
+    <div class="white-bg">
+      <h4>{{ modalRoom.title }}</h4>
+      <p>{{ modalRoom.content }}</p>
+      <input v-model.number="month">
+      <p>{{ month }}개월 가격 {{ month * modalRoom.price }}원</p>
+      <button @click="$emit('closeModal')">닫기</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Modal',
+  props: {
+    modal_status : Boolean,
+    modalRoom : Object
+  },
+  data() {
+    return {
+      month : 1
+    }
+  },
+  watch : {
+    month(input, result) {
+      if (isNaN(input)) {
+        alert("잘못된 입력");
+        this.month = result;
+      }
+      if (input > 12) {
+        alert("13이상 입력 X");
+        this.month = result;
+      }
+    }
+  },
+  updated() {
+    if (this.month === 2) {
+      alert("2 업데이트");
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
